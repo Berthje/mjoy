2cc10cb043f16d92d742a4e4863011403158c6db
Layton Berth <laytonbrth@gmail.com>
2024-10-16 00:54:58 +0200

    Refactor tailwind.config.ts to add custom color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,14 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: 'rgba(82, 39, 66, 1)', // M-JOY Purple
+        shade_primary: 'rgba(82, 39, 66, 0.3)', // M-JOY Purple Shade with 30% opacity
+        secondary: 'rgba(209, 165, 56, 1)', // M-JOY Orange
+        tertiary: 'rgba(237, 206, 75, 1)', // M-JOY Yellow
+        gray_title: 'rgba(51, 51, 51, 1)', // Gray 1 (Title)
+        gray_description: 'rgba(79, 79, 79, 1)', // Gray 2 (Description)
+        border_button: 'rgba(59, 72, 17, 1)', // Green Button Border
+        white: 'rgba(255, 255, 255, 1)', // White
       },
     },
   },
