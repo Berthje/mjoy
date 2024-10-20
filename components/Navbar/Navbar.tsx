0368a47a4f47a3abacfd2d0a7556d1edf02793ed
Layton Berth <laytonbrth@gmail.com>
2024-10-20 23:45:42 +0200

    Refactor font imports and update Navbar component

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -54,7 +54,7 @@ const Navbar = () => {
                                 height="100"
                                 alt="mjoy logo"
                             />
-                            <div className="cursor-pointer">
+                            <div className="cursor-pointer hover:opacity-75">
                                 <Image
                                     src="/close-menu.svg"
                                     width="35"
@@ -65,40 +65,74 @@ const Navbar = () => {
                         </div>
                         <ul className="text-secondary space-y-2 font-helvetica font-semibold uppercase text-[3rem] tracking-wide">
                             <li>
-                                <Link href="/" className="cursor-pointer ease-in-out duration-150 hover:opacity-75 hover:tracking-wider">Bistro</Link>
+                                <Link
+                                    href="/"
+                                    className="cursor-pointer ease-in-out duration-150 hover:opacity-75 hover:tracking-wider"
+                                >
+                                    Bistro
+                                </Link>
                             </li>
                             <li>
-                                <Link href="/" className="cursor-pointer ease-in-out duration-150 hover:opacity-75 hover:tracking-wider">Tearoom</Link>
+                                <Link
+                                    href="/"
+                                    className="cursor-pointer ease-in-out duration-150 hover:opacity-75 hover:tracking-wider"
+                                >
+                                    Tearoom
+                                </Link>
                             </li>
                             <li>
-                                <Link href="/" className="cursor-pointer ease-in-out duration-150 hover:opacity-75 hover:tracking-wider">Zondags Ontbijt</Link>
+                                <Link
+                                    href="/"
+                                    className="cursor-pointer ease-in-out duration-150 hover:opacity-75 hover:tracking-wider"
+                                >
+                                    Zondags Ontbijt
+                                </Link>
                             </li>
                             <li>
-                                <Link href="/" className="cursor-pointer ease-in-out duration-150 hover:opacity-75 hover:tracking-wider">Evenementen</Link>
+                                <Link
+                                    href="/"
+                                    className="cursor-pointer ease-in-out duration-150 hover:opacity-75 hover:tracking-wider"
+                                >
+                                    Evenementen
+                                </Link>
                             </li>
                             <li>
-                                <Link href="/" className="cursor-pointer ease-in-out duration-150 hover:opacity-75 hover:tracking-wider">Take Away</Link>
+                                <Link
+                                    href="/"
+                                    className="cursor-pointer ease-in-out duration-150 hover:opacity-75 hover:tracking-wider"
+                                >
+                                    Take Away
+                                </Link>
                             </li>
                             <li className="pt-6">
-                                <Link href="/" className="cursor-pointer ease-in-out duration-150 hover:opacity-75 hover:tracking-wider text-white">Reserveren?</Link>
+                                <Link
+                                    href="/"
+                                    className="cursor-pointer ease-in-out duration-150 hover:opacity-75 hover:tracking-wider text-white"
+                                >
+                                    Reserveren?
+                                </Link>
                             </li>
                         </ul>
                         <ul className="flex space-x-4 items-center mt-auto">
                             <li className="cursor-pointer hover:opacity-75">
-                                <Image
-                                    src="/facebook-icon.svg"
-                                    width="35"
-                                    height="35"
-                                    alt="facebook"
-                                />
+                                <a href="https://www.facebook.com/MJOYVeldegem/" target="_blank">
+                                    <Image
+                                        src="/facebook-icon.svg"
+                                        width="35"
+                                        height="35"
+                                        alt="facebook"
+                                    />
+                                </a>
                             </li>
                             <li className="cursor-pointer hover:opacity-75">
-                                <Image
-                                    src="/instagram-icon.svg"
-                                    width="35"
-                                    height="35"
-                                    alt="instagram"
-                                />
+                                <a href="https://www.instagram.com/mjoyveldegem/" target="_blank">
+                                    <Image
+                                        src="/instagram-icon.svg"
+                                        width="35"
+                                        height="35"
+                                        alt="instagram"
+                                    />
+                                </a>
                             </li>
                         </ul>
                     </div>
